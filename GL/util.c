9f3adc099ade682bb5f49bc2a9bf9f5be33a9fdc
Luke Benstead <kazade@gmail.com>
2019-11-12 17:28:39 +0000

    Fix a bad reference

--- GL/util.c
+++ GL/util.c
@@ -1,15 +1,15 @@
 #include "../include/glkos.h"
 
 void APIENTRY glVertexPackColor3fKOS(GLVertexKOS* vertex, float r, float g, float b) {
-    vertex->color[3] = 255;
-    vertex->color[2] = (r * 255.0f);
-    vertex->color[1] = (g * 255.0f);
-    vertex->color[0] = (b * 255.0f);
+    vertex->bgra[3] = 255;
+    vertex->bgra[2] = (r * 255.0f);
+    vertex->bgra[1] = (g * 255.0f);
+    vertex->bgra[0] = (b * 255.0f);
 }
 
 void APIENTRY glVertexPackColor4fKOS(GLVertexKOS* vertex, float r, float g, float b, float a) {
-    vertex->color[3] = (a * 255.0f);
-    vertex->color[2] = (r * 255.0f);
-    vertex->color[1] = (g * 255.0f);
-    vertex->color[0] = (b * 255.0f);
+    vertex->bgra[3] = (a * 255.0f);
+    vertex->bgra[2] = (r * 255.0f);
+    vertex->bgra[1] = (g * 255.0f);
+    vertex->bgra[0] = (b * 255.0f);
 }
