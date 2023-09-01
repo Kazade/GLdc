246cb997dabe0ea377d5f052c78eabdcfcf198eb
Luke Benstead <kazade@gmail.com>
2023-09-01 20:22:19 +0100

    Add optional dcprof to prof_texture_upload

--- samples/prof_texture_upload/main.c
+++ samples/prof_texture_upload/main.c
@@ -4,6 +4,7 @@
 
 #ifdef __DREAMCAST__
 #include <kos.h>
+#include "../profiler.h"
 #endif
 
 #include <GL/gl.h>
@@ -11,11 +12,13 @@
 
 #include "image.h"
 
+#define PROFILE 0
 
 int main(int argc, char* argv[]) {
     (void) argc;
     (void) argv;
 
+    fprintf(stdout, "Initializing\n");
     glKosInit();
     glClearColor(0.5f, 0.0f, 0.5f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -32,6 +35,13 @@ int main(int argc, char* argv[]) {
 
     fprintf(stderr, "Starting test run...\n");
 
+#ifdef __DREAMCAST__
+#if PROFILE
+    profiler_init("/pc/gmon.out");
+    profiler_start();
+#endif
+#endif
+
     while((end - start) < 5) {
         glTexImage2D(
             GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, header_data
@@ -40,6 +50,13 @@ int main(int argc, char* argv[]) {
         ++counter;
         end = time(NULL);
     }
+
+#ifdef __DREAMCAST__
+#if PROFILE
+    profiler_stop();
+    profiler_clean_up();
+#endif
+#endif
 
     fprintf(stderr, "Called glTexImage2D %d times (%.4f per call)\n", counter, (float)(end - start) / (float)(counter));
 
