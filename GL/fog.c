56f5b24d851c328b090cffdc7c9b223b51c991a0
Falco Girgis <gyrovorbis@gmail.com>
2025-02-18 21:52:19 -0600

    Fixed OpenGL Fog implementation + Added New Sample
    - Redid fog.c OpenGL fog back-end implementation
        * only reconfigure fog values when changed
        * properly clamp between 0.0f and 1.0f colors
        * error out on negative densities
        * separate fog statics became single struct
    - Added Nehe16 example (adapted from KGL)
        * Made spinning much faster (so you can see it better)
        * fixed a few issues with KGL code (invalid GL enums, lol)
        * swapped to new controller API
        * added #ifdef for non-DC file paths

--- GL/fog.c
+++ GL/fog.c
@@ -1,73 +1,139 @@
 
+/*
+ TODO: glGetX() fog values
+*/
+
 #include <limits.h>
 #include "private.h"
 
-static GLfloat FOG_START = 0.0f;
-static GLfloat FOG_END = 1.0f;
-static GLfloat FOG_DENSITY = 1.0f;
-static GLenum FOG_MODE = GL_EXP;
-static GLfloat FOG_COLOR [] = {0.0f, 0.0f, 0.0f, 0.0f};
+static struct {
+    GLfloat START;
+    GLfloat END;
+    GLfloat DENSITY;
+    GLenum  MODE;
+    GLfloat COLOR[4];
+} FOG = {
+    0.0f, 1.0f, 1.0f, GL_EXP, { 0.0f, 0.0f, 0.0f, 0.0f }
+};
 
-static void updatePVRFog() {
-    if(FOG_MODE == GL_LINEAR) {
-        GPUSetFogLinear(FOG_START, FOG_END);
-    } else if(FOG_MODE == GL_EXP) {
-        GPUSetFogExp(FOG_DENSITY);
-    } else if(FOG_MODE == GL_EXP2) {
-        GPUSetFogExp2(FOG_DENSITY);
+static void updatePVRFog(void) {
+    switch(FOG.MODE) {
+        case GL_LINEAR:
+            GPUSetFogLinear(FOG.START, FOG.END);
+            break;
+
+        case GL_EXP:
+            GPUSetFogExp(FOG.DENSITY);
+            break;
+
+        case GL_EXP2:
+            GPUSetFogExp2(FOG.DENSITY);
+            break;
     }
-    GPUSetFogColor(FOG_COLOR[3], FOG_COLOR[0], FOG_COLOR[1], FOG_COLOR[2]);
+
+    GPUSetFogColor(FOG.COLOR[3], FOG.COLOR[0], FOG.COLOR[1], FOG.COLOR[2]);
 }
 
-void APIENTRY glFogf(GLenum pname,  GLfloat param) {
+void APIENTRY glFogf(GLenum pname, GLfloat param) {
     switch(pname) {
-    case GL_FOG_MODE: {
-        FOG_MODE = (GLenum) param;
-        updatePVRFog();
-    } break;
-    case GL_FOG_DENSITY: {
-        FOG_DENSITY = param;
-        updatePVRFog();
-    } break;
-    case GL_FOG_START: {
-        FOG_START = param;
-        updatePVRFog();
-    } break;
-    case GL_FOG_END: {
-        FOG_END = param;
-        updatePVRFog();
-    } break;
-    case GL_FOG_INDEX:
-    default: {
-        _glKosThrowError(GL_INVALID_ENUM, __func__);
-    }
-    }
-}
+        case GL_FOG_DENSITY:
+            if(FOG.DENSITY != param) {
+                if(param < 0.0f)
+                    _glKosThrowError(GL_INVALID_VALUE, __func__);
+                else {
+                    FOG.DENSITY = param;
+                    updatePVRFog();
+                }
+            }
+            break;
 
-void APIENTRY glFogi(GLenum pname,  GLint param) {
-    glFogf(pname, (GLfloat) param);
-}
+        case GL_FOG_START:
+            if(FOG.START != param) {
+                FOG.START = param;
+                updatePVRFog();
+            }
+            break;
 
-void APIENTRY glFogfv(GLenum pname,  const GLfloat* params) {
-    if(pname == GL_FOG_COLOR) {
-        FOG_COLOR[0] = params[0];
-        FOG_COLOR[1] = params[1];
-        FOG_COLOR[2] = params[2];
-        FOG_COLOR[3] = params[3];
-        updatePVRFog();
-    } else {
-        glFogf(pname, *params);
+        case GL_FOG_END:
+            if(FOG.END != param) {
+                FOG.END = param;
+                updatePVRFog();
+            }
+            break;
+
+        default:
+            _glKosThrowError(GL_INVALID_ENUM, __func__);
     }
 }
 
-void APIENTRY glFogiv(GLenum pname,  const GLint* params) {
-    if(pname == GL_FOG_COLOR) {
-        FOG_COLOR[0] = ((GLfloat) params[0]) / (GLfloat) INT_MAX;
-        FOG_COLOR[1] = ((GLfloat) params[1]) / (GLfloat) INT_MAX;
-        FOG_COLOR[2] = ((GLfloat) params[2]) / (GLfloat) INT_MAX;
-        FOG_COLOR[3] = ((GLfloat) params[3]) / (GLfloat) INT_MAX;
-        updatePVRFog();
-    } else {
-        glFogi(pname, *params);
+void APIENTRY glFogi(GLenum pname, GLint param) {
+    switch(pname) {
+        case GL_FOG_DENSITY:
+        case GL_FOG_START:
+        case GL_FOG_END:
+            glFogf(pname, (GLfloat)param);
+            break;
+
+        case GL_FOG_MODE:
+            if(FOG.MODE != param) {
+                switch(param) {
+                    case GL_LINEAR:
+                    case GL_EXP:
+                    case GL_EXP2:
+                        FOG.MODE = param;
+                        updatePVRFog();
+                        break;
+
+                    default:
+                        _glKosThrowError(GL_INVALID_ENUM, __func__);
+                }
+            }
+            break;
+
+        case GL_FOG_INDEX:
+        default:
+            _glKosThrowError(GL_INVALID_ENUM, __func__);
+    }
+}
+
+void APIENTRY glFogfv(GLenum pname, const GLfloat* params) {
+    switch(pname) {
+        case GL_FOG_COLOR: {
+            GLfloat color[] = {
+                CLAMP(params[0], 0.0f, 1.0f),
+                CLAMP(params[1], 0.0f, 1.0f),
+                CLAMP(params[2], 0.0f, 1.0f),
+                CLAMP(params[3], 0.0f, 1.0f)
+            };
+
+            if(memcmp(color, FOG.COLOR, sizeof(float) * 4)) {
+                memcpy(FOG.COLOR, color, sizeof(float) * 4);
+                updatePVRFog();
+            }
+            break;
+        }
+
+        default:
+            glFogf(pname, *params);
+    }
+}
+
+void APIENTRY glFogiv(GLenum pname, const GLint* params) {
+    switch(pname) {
+        case GL_FOG_COLOR: {
+            GLfloat color[] = {
+                (GLfloat)params[0] / (GLfloat)INT_MAX,
+                (GLfloat)params[1] / (GLfloat)INT_MAX,
+
+                (GLfloat)params[2] / (GLfloat)INT_MAX,
+                (GLfloat)params[3] / (GLfloat)INT_MAX,
+            };
+
+            glFogfv(pname, color);
+            break;
+        }
+
+        default:
+            glFogi(pname, *params);
     }
 }
